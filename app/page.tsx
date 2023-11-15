93698493da2e4752b3ac7e8b5771f00d25f63718
Christian Arevalo <tiancris200@gmail.com>
2023-11-15 14:33:56 -0500

    :lipstick: Added footer section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import PredictImage from "@/components/PredictImage";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="grid w-full place-content-center gap-10 overflow-hidden">
-      <Hero />
-      <PredictImage />
-    </main>
+    <>
+      <main className="grid w-full place-content-center gap-10 overflow-hidden">
+        <Hero />
+        <PredictImage />
+      </main>
+      <Footer />
+    </>
   );
 }
